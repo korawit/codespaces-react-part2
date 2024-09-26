5da6000c87729b8518ef0626d0f56c1fcc66a2a7
korawit <korawit.orkphol@gmail.com>
2024-09-26 08:31:23 +0000

    Initial Version

---
+++ src/Bmi.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from "react";
+function BmiText({bmi}){
+    if(bmi<18.5) return <h1>Underweight!</h1>;
+    if(bmi>30) return <h1>Overweight</h1>;
+    return <h1>Normal</h1>;
+}
+export default function Bmi(){
+    const W_input = useRef(null);
+    const H_input = useRef(null);
+    const [bmi,setBmi]=useState(0.00);
+    function handleClick(){
+        let weight=W_input.current.value;
+        let height=H_input.current.value/100;
+        setBmi(weight/(height*height));
+    }
+    return (<>Weight:<input ref={W_input}/> kg.<br/>height:<input ref={H_input}/>cm.<br/>
+    <button onClick={handleClick}>calculate</button><br/>
+    BMI:{bmi.toFixed(2)}
+    <BmiText bmi={bmi}/></>);
+}
